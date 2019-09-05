50d144deeb00dcfa335f9592f895e20b1c0fc39b
Jacob Archambault <jacob.archambault@waystar.com>
2019-09-05 15:19:25 -0400

    Add ProductsListViewModel to SportsStore WebUI, and Update Product controller's ViewResult() method to return a ProductsListViewModel. IMPORTANT: This breaks the project. Do not revert here.

--- SportsStore.WebUI/Controllers/ProductController.cs
+++ SportsStore.WebUI/Controllers/ProductController.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using SportsStore.Domain.Abstract;
 using SportsStore.Domain.Entities;
+using SportsStore.WebUI.Models;
 
 namespace SportsStore.WebUI.Controllers
 {
@@ -20,10 +21,20 @@ namespace SportsStore.WebUI.Controllers
 
         public ViewResult List(int page = 1)
         {
-            return View(repository.Products
+            ProductListViewModel model = new ProductListViewModel
+            {
+                Products = repository.Products
                 .OrderBy(p => p.ProductID)
-                .Skip((page -1) * PageSize)
-                .Take(PageSize));
+                .Skip((page - 1) * PageSize)
+                .Take(PageSize),
+                PagingInfo = new PagingInfo
+                {
+                    CurrentPage = page,
+                    ItemsPerPage = PageSize,
+                    TotalItems = repository.Products.Count()
+                }
+            };
+            return View(model);
         }
     }
 }
