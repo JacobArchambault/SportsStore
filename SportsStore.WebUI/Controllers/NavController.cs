80d4bc1fb74164b4351607541b570b5cd656d91b
Jacob Archambault <jacob.archambault@waystar.com>
2019-09-06 12:57:01 -0400

    Generate Category list from NavController (breaks project temporarily)

--- SportsStore.WebUI/Controllers/NavController.cs
+++ SportsStore.WebUI/Controllers/NavController.cs
@@ -1,17 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
+using SportsStore.Domain.Abstract;
 
 namespace SportsStore.WebUI.Controllers
 {
     public class NavController : Controller
     {
-        // GET: Nav
-        public string Menu()
+        private IProductRepository repository;
+
+        public NavController(IProductRepository repo)
         {
-            return "Hello from NavController";
+            repository = repo;
+        }
+        // GET: Nav
+        public PartialViewResult Menu()
+        {
+            IEnumerable<string> categories = repository.Products
+                .Select(x => x.Category)
+                .Distinct()
+                .OrderBy(x => x);
+            return PartialView(categories);
         }
     }
 }
