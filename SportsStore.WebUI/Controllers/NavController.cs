5c4d5134227f7ea9bc5817667255f3a291c0f6b0
Jacob Archambault <jacob.archambault@waystar.com>
2019-09-10 14:04:43 -0400

    Add FlexMenu to replace Menu and MenuHorizontal. Fix menu navigation so clicking on home in mobile view doesn't give 404 error. Project working.

--- SportsStore.WebUI/Controllers/NavController.cs
+++ SportsStore.WebUI/Controllers/NavController.cs
@@ -15,7 +15,7 @@ namespace SportsStore.WebUI.Controllers
         }
 
         // GET: Nav
-        public PartialViewResult Menu(string category = null, bool horizontalLayout = false)
+        public PartialViewResult Menu(string category = null)
         {
             ViewBag.SelectedCategory = category;
 
@@ -24,8 +24,7 @@ namespace SportsStore.WebUI.Controllers
                 .Distinct()
                 .OrderBy(x => x);
 
-            string viewName = horizontalLayout ? "MenuHorizontal" : "Menu";
-            return PartialView(viewName, categories);
+            return PartialView("FlexMenu", categories);
         }
     }
 }
